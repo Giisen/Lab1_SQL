a8cbbf0916b2dbc9b34349e3ed1fc6c5553d3129
giisen <kristerekelund@me.com>
2022-11-16 10:15:22 +0100

    Update Lab1_SQL.sql
    lite mer data

--- Lab1_SQL.sql
+++ Lab1_SQL.sql
@@ -2,35 +2,6 @@
 use Bokhandel;
 
 
-
---create table Författare(
---ID int IDENTITY(1,1) primary key,
---Förnamn nvarchar(max) not null,
---Efternamn nvarchar(max) not null,
---Födelsedatum date not null,
---);
-
---create table Bokform(
---FormID int IDENTITY(1,1) primary key,
---BokForm nvarchar not null
---);
- 
-
---create table Böcker(
---ISBN13 nvarchar(32) primary key,
---Titel nvarchar(max) not null,
---Språk nvarchar(max) not null,
---Pris int not null,
---Utgivningsdatum date not null,
---FörfattareID int not null,
---FormID int not null,
---AntalSidor int,
---Genre nvarchar,
---constraint FK_FörfattareID foreign key (FörfattareID) references Författare(ID) on delete cascade on update cascade,
---constraint FK_FormID foreign key (FormID) references Bokform(FormID) on delete cascade on update cascade
---);
-
-
 --create table Personal(
 --PersonalID int IDENTITY(1,1) primary key,
 --Förnamn nvarchar(max) not null,
@@ -63,8 +34,6 @@ use Bokhandel;
 --);
 
 
-
-
 --insert into Butiker
 --(ButiksNamn,Gatuadress,Postnummer,Postort,Butikschef)
 --Values
@@ -72,9 +41,6 @@ use Bokhandel;
 --('Åsa','Strandvägen 12','41719','Åsa',4),
 --('Stockholm','Paradgatan 8','51317','Stockholm',8)
 --;
-
-select * from butiker;
-
 
 --insert into Personal
 --(Förnamn,Efternamn,AFormID,A_From,A_Tom,ButikID)
@@ -89,6 +55,42 @@ select * from butiker;
 --('Olof','Nilsson',1,'20000201','99991231',3);
 
 
+
+--create table Författare(
+--ID int IDENTITY(1,1) primary key,
+--Förnamn nvarchar(max) not null,
+--Efternamn nvarchar(max) not null,
+--Födelsedatum date not null,
+--);
+
+--create table Bokform(
+--FormID int IDENTITY(1,1) primary key,
+--BokForm nvarchar(max) not null
+--);
+
+
+ --insert into Bokform
+ --(Bokform)
+ --Values
+ --('Inbunden'),
+ --('Pocket'),
+ --('Audiobok'),
+ --('Ebok');
+
+
+--create table Böcker(
+--ISBN13 nvarchar(32) primary key,
+--Titel nvarchar(max) not null,
+--Språk nvarchar(max) not null,
+--Pris int not null,
+--Utgivningsdatum date not null,
+--FörfattareID int not null,
+--FormID int not null,
+--AntalSidor int,
+--Genre nvarchar(max),
+--constraint FK_FörfattareID foreign key (FörfattareID) references Författare(ID) on delete cascade on update cascade,
+--constraint FK_FormID foreign key (FormID) references Bokform(FormID) on delete cascade on update cascade
+--);
 
 
 
@@ -119,12 +121,10 @@ select * from butiker;
 --('Grishamn', 'John','19550208'),
 --('Sten', 'Viveca','19590618');
 
---insert into Böcker (ISBN13,Titel,Språk,Pris,Utgivningsdatum,FörfattareID,FormID,AntalSidor,Genre)
---values(
---'9780099537090','The Firm','Engelska',99,'20101028',2,1,496,'Thriller'),
---('9789175035505','I farans riktning','Svenska',69,'20160616',3,2,186,'Roman');
-
-
+insert into Böcker (ISBN13,Titel,Språk,Pris,Utgivningsdatum,FörfattareID,FormID,AntalSidor,Genre)
+values(
+'9780099537090','The Firm','Engelska',99,'20101028',2,1,496,'Thriller'),
+('9789175035505','I farans riktning','Svenska',69,'20160616',3,2,186,'Roman');
 
 
 
@@ -136,4 +136,4 @@ select * from butiker;
 --add constraint FK_Anställningsform foreign key (AFormID) references Anställningsform(AFormID);
 
 --alter table Butiker
---constraint FK_Butikschef foreign key (Butikschef) references Personal(PersonalID) on delete cascade on update cascade;
+--add constraint FK_Butikschef foreign key (Butikschef) references Personal(PersonalID) on delete cascade on update cascade;
