34458da587a3f501cb1186091d7a44c8f754e174
giisen <kristerekelund@me.com>
2022-11-16 09:07:19 +0100

    more changes

--- Lab1_SQL.sql
+++ Lab1_SQL.sql
@@ -1,40 +1,50 @@
 --create database Bokhandel;
 use Bokhandel;
 
+
+
 --create table Författare(
 --ID int IDENTITY(1,1) primary key,
---Förnamn nvarchar not null,
---Efternamn nvarchar not null,
+--Förnamn nvarchar(max) not null,
+--Efternamn nvarchar(max) not null,
 --Födelsedatum date not null,
 --);
 
 --create table Bokform(
 --FormID int IDENTITY(1,1) primary key,
---FormNamn nvarchar not null
+--BokForm nvarchar not null
 --);
  
 
 --create table Böcker(
---ISBN13 int primary key,
---Titel nvarchar not null,
---Språk nvarchar not null,
+--ISBN13 nvarchar(32) primary key,
+--Titel nvarchar(max) not null,
+--Språk nvarchar(max) not null,
 --Pris int not null,
 --Utgivningsdatum date not null,
 --FörfattareID int not null,
 --FormID int not null,
---constraint FK_FörfattareID foreign key (FörfattareID) references Författare(ID),
---constraint FK_Form foreign key (FormID) references Bokform(FormID)
+--AntalSidor int,
+--Genre nvarchar,
+--constraint FK_FörfattareID foreign key (FörfattareID) references Författare(ID) on delete cascade on update cascade,
+--constraint FK_FormID foreign key (FormID) references Bokform(FormID) on delete cascade on update cascade
 --);
+
 
 --create table Personal(
 --PersonalID int IDENTITY(1,1) primary key,
 --Förnamn nvarchar not null,
 --Efternamn nvarchar not null,
---Anställningstyp int not null,
+--Anställningsform int not null,
 --A_From date not null,
 --A_Tom date not null,
 --ButikID int);
 
+
+--create table Anställningsform
+--(AFormID int IDENTITY(1,1) primary key,
+--Antställningsform nvarchar(max)
+--);
 
 
 --create table Butiker(
@@ -44,39 +54,55 @@ use Bokhandel;
 --Postnummer nvarchar not null,
 --Postort nvarchar not null,
 --Butikschef int not null
---constraint FK_Butikschef foreign key (Butikschef) references Personal(PersonalID)
+--constraint FK_Butikschef foreign key (Butikschef) references Personal(PersonalID) on delete cascade on update cascade
 --);
 
 --Alter table Personal
---add constraint FK_Anställning foreign key (ButikID) references Butiker(ButikID);
+--add constraint FK_Anställning foreign key (ButikID) references Butiker(ButikID); --on delete cascade on update cascade;
+
+--alter table personal
+--add constraint FK_Anställningsform foreign key (AFormID) references Anställningsform(AFormID);
+
+insert into Personal
+(Förnamn,Efternamn,Anställningstyp,A_From,A_Tom,ButikID)
+Values
+('Krister','Ekelund','Tillsvidare'
+
+
+
+
 
 
 --create table LagerSaldo(
 --ButikID int not null,
---ISBN int not null,
+--ISBN13 nvarchar(32) not null,
 --Antal int not null,
 --FormID int not null
---constraint FK_ButikID foreign key (ButikID) references Butiker(ButikID),
---constraint FK_ISBN foreign key (ISBN) references Böcker(ISBN13),
---Primary Key (ButikID,ISBN)
+--constraint FK_ButikID foreign key (ButikID) references Butiker(ButikID) on delete cascade on update cascade,
+--constraint FK_ISBN foreign key (ISBN13) references Böcker(ISBN13) on delete cascade on update cascade,
+--Primary Key (ButikID,ISBN13)
 --);
 
 
 
 --create table Förlag(
---ISBN13 int not null,
+--ISBN13 nvarchar(32) not null,
 --FörlagsNamn nvarchar not null,
 --Primary Key (ISBN13),
---foreign key (ISBN13) references Böcker(ISBN13) 
+--foreign key (ISBN13) references Böcker(ISBN13) on delete cascade on update cascade
 --); 
 
 
---Ändringar gjorda efter skapade tabeller:
 
---Alter table Böcker
---add AntalSidor int;
+--insert into Författare(Förnamn,Efternamn,Födelsedatum)
+--Values
+--('Brown', 'Dan','19640622'),
+--('Grishamn', 'John','19550208'),
+--('Sten', 'Viveca','19590618');
 
-
-
+--insert into Böcker (ISBN13,Titel,Språk,Pris,Utgivningsdatum,FörfattareID,FormID,AntalSidor,Genre)
+--values(
+--'9780099537090','The Firm','Engelska',99,'20101028',2,1,496,'Thriller'),
+--('9789175035505','I farans riktning','Svenska',69,'20160616',3,2,186,'Roman');
 
 
