d61640ce2bf330d2862f7a2feb594a0961106a47
giisen <kristerekelund@me.com>
2022-11-22 14:05:56 +0100

    Flyttabok troligen klar

--- FlyttaBok.sql
+++ FlyttaBok.sql
@@ -1,3 +1,5 @@
+Begin transaction;
+use Bokhandel
 
 Declare @FrånButik int
 set @FrånButik=1;
@@ -5,26 +7,21 @@ set @FrånButik=1;
 Declare @TillButik int
 set @TillButik=2;
 
-Declare @ISBN13 nvarchar(max)
-set @ISBN13=0000000000000;
+Declare @ISBN13 nvarchar(13)
+set @ISBN13='9780099537090';
 
 Declare @FlyttaAntal int
 set @FlyttaAntal=1;
 
-
-Begin transaction;
-use Bokhandel
-Alter procedure FlyttaBok
-
+GO:
 update LagerSaldo
-
 set Antal = Antal-@FlyttaAntal
---set Antal = Antal+@FlyttaAntal
-
 where ButikID=@FrånButik and ISBN13 = @ISBN13
 
---and where ButikId=@TillButik
 
+update LagerSaldo
+set Antal = Antal+@FlyttaAntal
+where ButikID=@TillButik and ISBN13 = @ISBN13
 
 commit;
 
