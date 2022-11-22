165ccec697113255fe8d35fef13db47596ecc8bb
giisen <kristerekelund@me.com>
2022-11-22 09:33:28 +0100

    Update Vy_TitlarPerFörfattare.sql

--- Vy_TitlarPerFörfattare.sql
+++ Vy_TitlarPerFörfattare.sql
@@ -6,22 +6,34 @@ as
 select 
 concat(t1.Förnamn,' ', t1.EfterNamn) as Namn,
 concat(DateDiff(year,t1.Födelsedatum,convert(date,getdate())),' År') as Ålder,
-count(distinct(t2.Titel)) as Titlar,
-format(sum(t2.pris*t3.Antal),'C0','se') as Lagervärde
---format(sum(t2.pris*t3.Antal),'C0','se') as Lagervärde
+count(distinct(t3.Titel)) as Titlar,
+format(sum(t3.pris*t4.Antal),'C0','se') as Lagervärde
 
 from Författare t1 
 
-left join Böcker t2
-on t1.ID=t2.FörfattarID 
+join BöckerFörfattareJunction t2
+on t1.ID=t2.FörfattareID 
 
-left join LagerSaldo t3
-on t2.ISBN13=t3.ISBN13
+join Böcker t3
+on t3.ISBN13=t2.ISBN13
+
+join LagerSaldo t4
+on t2.ISBN13=t4.ISBN13
 
 group by
 concat(t1.Förnamn,' ', t1.EfterNamn),
 concat(DateDiff(year,t1.Födelsedatum,convert(date,getdate())),' År');
 
+select
+t1.ISBN13,
+t1.Titel,
+t3.Förnamn,
+t3.Efternamn
+from Böcker t1
+join BöckerFörfattareJunction t2
+on t1.ISBN13=t2.ISBN13
 
+join Författare t3
+on t2.FörfattareID=t3.ID
 
 
