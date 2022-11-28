0625a1d1d14fa540f90e5ca536ddc8955492f279
giisen <kristerekelund@me.com>
2022-11-28 15:45:17 +0100

    Update Vy_TitlarPerFörfattare.sql

--- Vy_TitlarPerFörfattare.sql
+++ Vy_TitlarPerFörfattare.sql
@@ -1,15 +1,13 @@
 use Bokhandel;
 --drop view [TitlarPerFörfattare] 
 GO
-create view [TitlarPerFörfattare] 
+Create view [TitlarPerFörfattare] 
 as
 select
 concat(t1.Förnamn,' ', t1.EfterNamn) as Namn,
 concat(DateDiff(year,t1.Födelsedatum,convert(date,getdate())),' År') as Ålder,
 count(distinct(t3.Titel)) as Titlar,
-format(sum(t3.pris*t4.Antal),'C0','se') as Lagervärde,
-Convert(int,sum(t3.pris*t4.Antal)) as Lagervärde2,
-concat(sum(t3.pris*t4.Antal),' Kr') as Lagervärde3
+format(sum(t3.pris*t4.Antal),'C0','se') as Lagervärde
 
 from Författare t1 
 
@@ -24,7 +22,5 @@ on t2.ISBN13=t4.ISBN13
 
 group by
 concat(t1.Förnamn,' ', t1.EfterNamn),
-concat(DateDiff(year,t1.Födelsedatum,convert(date,getdate())),' År')
-order by Lagervärde2 desc
-;
+concat(DateDiff(year,t1.Födelsedatum,convert(date,getdate())),' År');
 
