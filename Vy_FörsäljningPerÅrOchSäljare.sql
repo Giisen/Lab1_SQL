811f3464d51e9abad039daf3a0c8f3e202e504ce
giisen <kristerekelund@me.com>
2022-11-29 10:02:24 +0100

    Update Vy_FörsäljningPerÅrOchSäljare.sql

--- Vy_FörsäljningPerÅrOchSäljare.sql
+++ Vy_FörsäljningPerÅrOchSäljare.sql
@@ -5,15 +5,21 @@ create view [FörsäljningPerÅrOchSäljare]
 as
 select distinct
 concat(t2.Förnamn,' ',t2.EfterNamn) as Namn,
-year(t1.OrderTid) as År,
-sum(t1.Antal) as Antal,
-format(sum(t1.Antal*t1.Styckpris),'C0','se') as Summa
+year(t1.Tid) as År,
+sum(t3.Antal) as Antal,
+format(sum(t3.Antal*t3.Styckpris),'C0','se') as Summa
 
 
-from Ordrar t1
+from OrdrarInfo t1
 join Personal t2
 on t1.PersonalID=t2.PersonalID
 
+join Ordrar t3
+on t1.OrderID=t3.OrderID
+
 group by 
 concat(t2.Förnamn,' ',t2.EfterNamn),
-year(OrderTid);
+year(t1.Tid);
+
+
+select * from FörsäljningPerÅrOchSäljare
