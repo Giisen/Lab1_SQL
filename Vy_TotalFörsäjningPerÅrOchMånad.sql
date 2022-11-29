ff20a18d2d5b2eb9ad090a0e3d3059c3d78d1b60
giisen <kristerekelund@me.com>
2022-11-29 10:09:06 +0100

    Update Vy_TotalFörsäjningPerÅrOchMånad.sql

--- Vy_TotalFörsäjningPerÅrOchMånad.sql
+++ Vy_TotalFörsäjningPerÅrOchMånad.sql
@@ -5,17 +5,20 @@ create view [TotalFörsäljningPerÅrOchMånad]
 as
 select distinct
 t2.ButiksNamn,
-year(t1.OrderTid) as År,
-month(t1.OrderTid) as Månad,
-sum(t1.Antal) as Antal,
-format(sum(t1.Antal*t1.Styckpris),'C0','se') as Summa
+year(t1.Tid) as År,
+month(t1.Tid) as Månad,
+sum(t3.Antal) as Antal,
+format(sum(t3.Antal*t3.Styckpris),'C0','se') as Summa
 
 
-from Ordrar t1
+from OrdrarInfo t1
 join Butiker t2
 on t1.ButikID=t2.ButikID
 
+join Ordrar t3
+on t1.OrderID=t3.OrderID
+
 group by 
 t2.ButiksNamn,
-year(OrderTid),
-month(t1.OrderTid);
+year(Tid),
+month(t1.Tid);
